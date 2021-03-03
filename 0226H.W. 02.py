ff0581967d24d7956f7c6683e10da46376ad44bb
shearer0118 <76277722+shearer0118@users.noreply.github.com>
2021-03-03 19:01:40 +0800

    Add files via upload

---
+++ 0226H.W. 02.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  3 18:47:30 2021
+
+@author: USER
+"""
+
+for i in range(10,1,-1):
+    for a in range(1,i):
+        print(a,end=(""))
+    print("")
+        
+        
