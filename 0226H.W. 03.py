b346ce73277f69a38dd66662209da51fbdfaebf5
shearer0118 <76277722+shearer0118@users.noreply.github.com>
2021-03-03 19:07:01 +0800

    Add files via upload

---
+++ 0226H.W. 03.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  3 19:01:52 2021
+
+@author: USER
+"""
+total = 0
+number = int(input("請輸入數字:"))
+for i in range(1,number):
+    total += i
+print("總和:",total)
